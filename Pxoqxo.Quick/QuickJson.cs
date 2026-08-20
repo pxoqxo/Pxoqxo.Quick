fa9ef471b56e20cd0ce429d8066c85071cdd767b
pxoqxo <pxoqxo@atomicmail.io>
2026-08-20 12:48:09 +0530

    QuickJson added.

---
+++ Pxoqxo.Quick/QuickJson.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Pxoqxo.Quick
+{
+    public static class QuickJson
+    {
+        public static string? ToJson<T>(T model)
+        {
+            try
+            {
+                return JsonSerializer.Serialize<T>(model);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static T? FromJson<T>(string json)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<T>(json);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+    }
+}
